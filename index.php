f99a1cca7ff1299ab81f396a893050fe0b1aa2b1
OrbitaNET <orbitanet90@gmail.com>
2014-02-25 18:13:59 +0300

    header

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<title>Броско Фитнес - FoxBox.Франчайзинг</title>
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<link rel="stylesheet/less" href="/less/bootstrap.less?v=<?= time(); ?>">
+	<script type="text/javascript" src="/js/less-1.6.3.min.js"></script>
+	<link rel="apple-touch-icon" sizes="57x57" href="http://brosko.ru/apple-touch-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="http://brosko.ru/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="http://brosko.ru/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="http://brosko.ru/apple-touch-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="http://brosko.ru/apple-touch-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="http://brosko.ru/apple-touch-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="http://brosko.ru/apple-touch-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="http://brosko.ru/apple-touch-icon-152x152.png">
+	<link rel="icon" type="image/png" href="http://brosko.ru/favicon-196x196.png" sizes="196x196">
+	<link rel="icon" type="image/png" href="http://brosko.ru/favicon-160x160.png" sizes="160x160">
+	<link rel="icon" type="image/png" href="http://brosko.ru/favicon-96x96.png" sizes="96x96">
+	<link rel="icon" type="image/png" href="http://brosko.ru/favicon-32x32.png" sizes="32x32">
+	<link rel="icon" type="image/png" href="http://brosko.ru/favicon-16x16.png" sizes="16x16">
+    <link rel="shortcut icon" href="http://brosko.ru/favicon.ico">
+</head>
+<body>
+	<div class="col-md-12 col-lg-12 col-sm-12 col-xs-12 wrap">
+        <div class="header-font"></div>
+		<div class="wrapper">
+			<div class="header">
+                <div class="title pull-left">Броско Фитнес</div>
+                <div class="title-phone pull-left"><span class="glyphicon glyphicon-phone"></span>8 800 555 0850</div>
+                <div class="title-dropdown dropdown pull-right">
+                    <a data-toggle="dropdown" href="#">
+                        <span style="position: relative;">Понин Виталий Дмитриевич<sub>заместитель коммерческого директора</sub></span>
+                        <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="/cabinet/user/settings">
+                                <span class="glyphicon glyphicon-cog"></span>&nbsp;Персональная информация
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/site/logout" class="delete">
+                                <span class="glyphicon glyphicon-off exit"></span>&nbsp;Выход
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+			</div>
+			<div class="content">
+
+			</div>
+		</div>
+	</div>
+    <script type="text/javascript" src="/js/jquery-2.0.3.min.js"></script>
+    <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+</body>
+</html>
