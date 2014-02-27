b654a9461f8d943b9d037192c6fa5852ba9e27fc
OrbitaNET <orbitanet90@gmail.com>
2014-02-27 11:15:02 +0300

    vse

---
+++ end.php
@@ -0,0 +1,5 @@
+        </div>
+        <script type="text/javascript" src="/js/jquery-2.0.3.min.js"></script>
+        <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+    </body>
+</html>
--- franchisee_list.php
+++ franchisee_list.php
@@ -1,229 +1,227 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8" />
-	<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-	<title>Броско Фитнес - FoxBox.Франчайзинг</title>
-	<meta name="keywords" content="" />
-	<meta name="description" content="" />
-	<link rel="stylesheet/less" href="/less/bootstrap.less?v=<?= time(); ?>">
-	<script type="text/javascript" src="/js/less-1.6.3.min.js"></script>
-	<link rel="apple-touch-icon" sizes="57x57" href="http://brosko.ru/apple-touch-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="http://brosko.ru/apple-touch-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="http://brosko.ru/apple-touch-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="http://brosko.ru/apple-touch-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="http://brosko.ru/apple-touch-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="http://brosko.ru/apple-touch-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="http://brosko.ru/apple-touch-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="http://brosko.ru/apple-touch-icon-152x152.png">
-	<link rel="icon" type="image/png" href="http://brosko.ru/favicon-196x196.png" sizes="196x196">
-	<link rel="icon" type="image/png" href="http://brosko.ru/favicon-160x160.png" sizes="160x160">
-	<link rel="icon" type="image/png" href="http://brosko.ru/favicon-96x96.png" sizes="96x96">
-	<link rel="icon" type="image/png" href="http://brosko.ru/favicon-32x32.png" sizes="32x32">
-	<link rel="icon" type="image/png" href="http://brosko.ru/favicon-16x16.png" sizes="16x16">
-    <link rel="shortcut icon" href="http://brosko.ru/favicon.ico">
-</head>
-<body>
-	<div class="col-md-12 col-lg-12 col-sm-12 col-xs-12 wrap">
-        <div class="header-font"></div>
-		<div class="wrapper">
-			<div class="header">
-                <div class="title pull-left">Броско Фитнес</div>
-                <div class="title-phone pull-left"><span class="glyphicon glyphicon-phone"></span>8 800 555 0850</div>
-                <div class="title-dropdown dropdown pull-right">
-                    <a data-toggle="dropdown" href="#">
-                        <span style="position: relative;">Понин Виталий Дмитриевич<sub>заместитель коммерческого директора</sub></span>
-                        <span class="caret"></span>
+<?php require_once('begin.php');?>
+<table class="wrapper">
+    <tr>
+        <td class="main-sidebar">
+            <ul class="nav nav-pills nav-stacked">
+                <li class="thumbnail add" data-submenu="submenu-plus">
+                    <a href="/" class="show text-center">
+                        <i class="sidebar-plus"></i>
+                        <p>Добавить</p>
                     </a>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <a href="/cabinet/user/settings">
-                                <span class="glyphicon glyphicon-home"></span>Персональная информация
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/site/logout" class="delete">
-                                <span class="glyphicon glyphicon-off exit"></span>Выход
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-			</div>
-            <div class="main-sidebar">
-                <ul class="nav nav-pills nav-stacked">
-                    <li class="thumbnail add" data-submenu="submenu-plus">
-                        <a href="/" class="show text-center">
-                            <i class="sidebar-plus"></i>
-                            <p>Добавить</p>
-                        </a>
-                    </li>
-                    <li class="active thumbnail" data-submenu="submenu-desktop">
-                        <a href="/" class="show text-center">
-                            <i class="sidebar-desktop"></i>
-                            <p>Рабочий стол</p>
-                        </a>
-                    </li>
-                    <li class="thumbnail" data-submenu="submenu-common">
-                        <a href="/" class="show text-center">
-                            <i class="sidebar-common"></i>
-                            <p>Общее</p>
-                        </a>
-                    </li>
-                    <li class="thumbnail" data-submenu="submenu-company">
-                        <a href="/" class="show text-center">
-                            <i class="sidebar-company"></i>
-                            <p>Компания</p>
-                        </a>
-                    </li>
-                    <li class="thumbnail" data-submenu="submenu-news">
-                        <a href="/" class="show text-center">
-                            <i class="sidebar-news"></i>
-                            <p>Новости</p>
-                        </a>
-                    </li>
-                    <li class="thumbnail" data-submenu="submenu-guides">
-                        <a href="/" class="show text-center">
-                            <i class="sidebar-guides"></i>
-                            <p>Руководства</p>
-                        </a>
-                    </li>
-                    <li class="thumbnail" data-submenu="submenu-university">
-                        <a href="/" class="show text-center">
-                            <i class="sidebar-university"></i>
-                            <p>Университет</p>
-                        </a>
-                    </li>
-                    <li class="thumbnail" data-submenu="submenu-settings">
-                        <a href="/" class="show text-center">
-                            <i class="sidebar-settings"></i>
-                            <p>Настройки</p>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <div class="second-sidebar">
-                <ul class="nav nav-pills nav-stacked" id="submenu-plus">
-                    <li>
-                        <a href="/" class="show text-left">
-                            Сотрудника
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/" class="show text-left">
-                            Франчайзи
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/" class="show text-left">
-                            Потенциального франчайзи
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/" class="show text-left">
-                            Задачу
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/" class="show text-left">
-                            Комментарий к франчайзи
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/" class="show text-left">
-                            Сообщение
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/" class="show text-left">
-                            Новость
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/" class="show text-left">
-                            Руководство
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/" class="show text-left">
-                            Университет
-                        </a>
-                    </li>
-                 </ul>
-            </div>
-            <div id="content">
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb f<br>
-                dafzgkbanfdkbnjdfanb fap fap fap<br>
-            </div>
-            <br style="clear:both;" />
-		</div>
-	</div>
-    <script type="text/javascript" src="/js/jquery-2.0.3.min.js"></script>
-    <script type="text/javascript" src="/js/bootstrap.min.js"></script>
-</body>
-</html>
+                </li>
+                <li class="thumbnail" data-submenu="submenu-desktop">
+                    <a href="/" class="show text-center">
+                        <i class="sidebar-desktop"></i>
+                        <p>Рабочий стол</p>
+                    </a>
+                </li>
+                <li class="thumbnail" data-submenu="submenu-common">
+                    <a href="/" class="show text-center">
+                        <i class="sidebar-common"></i>
+                        <p>Общее</p>
+                    </a>
+                </li>
+                <li class="active thumbnail" data-submenu="submenu-company">
+                    <a href="/franchisee_list.php" class="show text-center">
+                        <i class="sidebar-company"></i>
+                        <p>Компания</p>
+                    </a>
+                </li>
+                <li class="thumbnail" data-submenu="submenu-news">
+                    <a href="/" class="show text-center">
+                        <i class="sidebar-news"></i>
+                        <p>Новости</p>
+                    </a>
+                </li>
+                <li class="thumbnail" data-submenu="submenu-guides">
+                    <a href="/" class="show text-center">
+                        <i class="sidebar-guides"></i>
+                        <p>Руководства</p>
+                    </a>
+                </li>
+                <li class="thumbnail" data-submenu="submenu-university">
+                    <a href="/" class="show text-center">
+                        <i class="sidebar-university"></i>
+                        <p>Университет</p>
+                    </a>
+                </li>
+                <li class="thumbnail" data-submenu="submenu-settings">
+                    <a href="/" class="show text-center">
+                        <i class="sidebar-settings"></i>
+                        <p>Настройки</p>
+                    </a>
+                </li>
+            </ul>
+        </td>
+        <td class="second-sidebar col-lg-2 col-md-2">
+            <ul class="nav nav-pills nav-stacked" id="submenu-plus">
+                <li>
+                    <a href="/" class="show text-left">
+                        Сотрудника
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="show text-left">
+                        Франчайзи
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="show text-left">
+                        Потенциального франчайзи
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="show text-left">
+                        Задачу
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="show text-left">
+                        Комментарий к франчайзи
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="show text-left">
+                        Сообщение
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="show text-left">
+                        Новость
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="show text-left">
+                        Руководство
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="show text-left">
+                        Университет
+                    </a>
+                </li>
+            </ul>
+            <ul class="nav nav-pills nav-stacked" id="submenu-common">
+                <li>
+                    <a href="/" class="show text-left">
+                        Лента событий
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="show text-left">
+                        Календарь
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="show text-left">
+                        Сообщения
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="show text-left">
+                        Задачи
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="show text-left">
+                        Уведомления
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="show text-left">
+                        Обращения
+                    </a>
+                </li>
+            </ul>
+            <ul class="nav nav-pills nav-stacked" id="submenu-company"  style="display: block;">
+                <li class="active">
+                    <a href="/" class="show text-left">
+                        Франчайзи
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="show text-left">
+                        Сотрудники
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="show text-left">
+                        Дни рождения
+                    </a>
+                </li>
+            </ul>
+            <ul class="nav nav-pills nav-stacked" id="submenu-news">
+                <li>
+                    <a href="/" class="show text-left">
+                        Все новости
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="show text-left">
+                        Новости франчайзинга
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="show text-left">
+                        Новости компании
+                    </a>
+                </li>
+            </ul>
+            <ul class="nav nav-pills nav-stacked" id="submenu-settings">
+                <li>
+                    <a href="/" class="show text-left">
+                        Логи
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="show text-left">
+                        Права доступа
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="show text-left">
+                        Справочники
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="show text-left">
+                        Страница «О компании»
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="show text-left">
+                        Настройки администратора
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="show text-left">
+                        Настройка тестов
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="show text-left">
+                        Настройка новостей
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="show text-left">
+                        Настройка руководств
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="show text-left">
+                        Настройка университетов
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="show text-left">
+                        Руководители компании
+                    </a>
+                </li>
+            </ul>
+        </td>
+        <td id="content">
+        </td>
+    </tr>
+</table>
+<?php require_once('end.php');?>
